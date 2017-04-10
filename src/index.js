231ef3926768db390605674cbd37f515bab80ecd
Logan Bernard <bernard.logan4@gmail.com>
2017-04-10 15:11:26 -0400

    use npm link to maintain as separate project

--- src/index.js
+++ src/index.js
@@ -1,93 +1,25 @@
-import process from 'process'
-const debug = require('debug')('kit')
-
-import { core } from './engines'
-
-//
-// kit
-// ==================================================
-// ==================================================
-//
+import test, { engine } from 'co-kit'
+// import engine from '../index'
+const debug = require('debug')('messenger')
 
 export default {
 
-  middleware: [],
-  engines: [],
-
-  config(config) {
-    this.middleware = config.middleware || []
-    this.engines = config.engines || []
-    return this
-  },
+  type: 'messenger',
 
   listen() {
-    const pkt = this.initPkt()
-    this.handleIn(pkt)
-    this.engines.map(engine =>
-      engine.listen()
-    )
+    setTimeout(this.emit, 1000)
+    setTimeout(this.emit, 5000)
+    // while(true) {
+    // }
+    return
   },
 
-  handleIn(pkt) {
-    core.on('in', async (event, engineType) => {
-      debug('EVENT IN')
-      debug(event, engineType)
-
-      const [ engine ] = this.filterEngines(engineType, this.engines)
-      pkt.in = { event }
-      pkt.out = {}
-      pkt.engine = engine
-
-      this.handleOut(await this.applyMiddleware(pkt))
-    })
+  emit(event) {
+    engine.core.emit('in', 1, 'messenger')
   },
 
-  async applyMiddleware(pkt) {
-    pkt.out.event = await applyRecursive(pkt, this.middlewareFns)
-    .catch(handleError)
-
-    return pkt
+  send(pkt) {
+    debug(pkt)
   },
 
-  handleOut(pkt) {
-    pkt.engine.send(pkt.out.event)
-  },
-
-  //
-  // Helper Functions
-  // ==================================================
-  // ==================================================
-  //
-
-  initPkt() {
-    return {}
-  },
-
-  filterEngines(engineType, engines) {
-    return engines.filter(engine => engine.type === engineType)
-  },
-
-  get middlewareFns() {
-    return this.middleware.slice(0)
-  },
-
-}
-
-// END kit
-// ==================================================
-// ==================================================
-//
-
-async function applyRecursive(event, fnQueue) {
-  if (fnQueue.length === 0) {
-    return event
-  }
-  else {
-    const fn = fnQueue.shift()
-    return await applyRecursive(fn(event), fnQueue)
-  }
-}
-
-function handleError(err) {
-  console.error(err)
 }
