9b673c50f73d14c455aef0248b40879aab41c868
Logan Bernard <bernard.logan4@gmail.com>
2017-04-11 10:57:43 -0400

    update to eventemitter // server

---
+++ src/events/index.js
@@ -0,0 +1,93 @@
+/**
+ *
+ * All Messenger event types with properties
+ *
+ *
+ */
+const eventProps = {
+  default: [
+    'sender',
+    'recipient',
+    'timestamp'
+  ],
+  message: [
+    'text',
+    'sticker_id',
+    'attachments',
+    'quick_reply',
+    'is_echo',
+    'seq',
+  ],
+  postback: [
+    'text',
+    'payload',
+    'referral',
+  ],
+  optin: [
+    'optin',
+  ],
+  delivery: [
+    'delivery',
+  ],
+  read: [
+    'read',
+  ],
+  referral: [
+    'referral',
+  ],
+  account_linking: [
+    'account_linking',
+  ]
+}
+
+/**
+ *
+ * Parse each event object into the form of:
+ *
+ *  {
+ *    type: String,
+ *    event: Object (raw Messenger Object),
+ *    [type]: Object (type specific event properties),
+ *    meta: Object
+ *  }
+ *
+ */
+export function parseEvents(entry) {
+  if (!entry) {
+    return []
+  }
+  else {
+    return entry.map(
+      messaging =>
+        messaging
+        .map(parseEventType)
+        .map(parseTypeProps)
+        .map(parseEventMeta)
+    )
+  }
+}
+
+const types = Object.keys(eventProps)
+
+function parseEventType(event) {
+  const [ type ] = types.filter(type => event[type])
+  return { type, event }
+}
+
+const parseTypeProps = ({ type, event }) => ({
+  type,
+  event,
+  [type]: event[type]
+})
+
+const parseEventMeta = (eventPacket) => {
+  const { event } = eventPacket
+  eventPacket['meta'] = {
+    sender: event.sender,
+    recipient: event.recipient,
+    timestamp: event.timestamp,
+  }
+  return eventPacket
+}
+
+export default eventProps
--- src/index.js
+++ src/index.js
@@ -1,25 +1,89 @@
-import test, { engine } from 'co-kit'
-// import engine from '../index'
+import EventEmitter from 'events'
+import { engine } from 'co-kit'
+import express from 'express'
+import bodyParser from 'body-parser'
+import catchPromiseErrors from 'async-error-catcher'
+
+// import { listen } from './server'
+// import { verifyRequestSignature } from './routes'
+import { parseEvents } from './events'
+
 const debug = require('debug')('messenger')
+class EngineMessenger extends EventEmitter {
 
-export default {
-
-  type: 'messenger',
+  constructor(config) {
+    super()
+    this.type = 'messenger'
+    this.APP_SECRET = config.APP_SECRET || ''
+    this.ACCESS_TOKEN = config.ACCESS_TOKEN || ''
+    this.VERIFY_TOKEN = config.VERIFY_TOKEN || ''
+  }
 
   listen() {
-    setTimeout(this.emit, 1000)
-    setTimeout(this.emit, 5000)
-    // while(true) {
-    // }
-    return
-  },
+    const app = express()
+    app.set('port', (process.env.PORT || 3434))
+    app.use(bodyParser.json({ verify: this.verifyRequestSignature }))
+    app.get('/', (req, res) => res.send('kit-engine-messenger is good to go!'))
+    app.get('/kit/engine/messenger', catchPromiseErrors(this.handleWebhookGet))
+    app.post('/kit/engine/messenger', catchPromiseErrors(this.handleWebhookPost))
+    app.use((err, req, res, next) => {
+      console.error(err.stack || err)
+      res.sendStatus(200)
+    })
+    app.listen(app.get('port'), function() {
+      console.log(`kit-engine-messenger: ${app.get('port')}`)
+    })
+  }
 
-  emit(event) {
-    engine.core.emit('in', 1, 'messenger')
-  },
+  handleWebhookGet(req, res) {
+    if (req.query['hub.verify_token'] === this.VERIFY_TOKEN) {
+      res.send(req.query['hub.challenge'])
+    }
+    else {
+      res.send('Error, wrong token')
+    }
+  }
+
+  handleWebhookPost(req, res) {
+    const events = parseEvents(req.body.entry)
+    events.map(this.emitEvent)
+  }
+
+  verifyRequestSignature(req, res, buf) {
+    let signature = req.headers["x-hub-signature"];
+    if (!signature) {
+      throw new Error('Unable to validate request signature!')
+    }
+    else {
+      let elements = signature.split('=');
+      let method = elements[0];
+      let signatureHash = elements[1];
+      let expectedHash = crypto.createHmac('sha1', this.APP_SECRET)
+      .update(buf)
+      .digest('hex');
+      if (signatureHash != expectedHash) {
+        throw new Error("Couldn't validate the request signature.");
+      }
+    }
+  }
+
+  // listen() {
+  //   setTimeout(this.emit, 1000)
+  //   setTimeout(this.emit, 5000)
+  //   // while(true) {
+  //   // }
+  //   return
+  // },
+
+  emitEvent(event) {
+    if (!event.type) throw new TypeError('incoming event has no type!')
+    this.emit(`in:${this.type}`, event)
+  }
 
   send(pkt) {
     debug(pkt)
-  },
+  }
 
 }
+
+export default EngineMessenger
