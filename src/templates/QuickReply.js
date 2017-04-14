1029db1756db1a4611c4a70459a72414630b803c
Logan Bernard <bernard.logan4@gmail.com>
2017-04-13 21:21:59 -0400

    add eslint // npm start

--- src/templates/QuickReply.js
+++
@@ -1,8 +0,0 @@
-export default class QuickReply {
-  constructor(props) {
-    this.content_type = props.content_type || null
-    this.title = props.title || null
-    this.payload = props.payload || null
-    this.image_url = props.image_url || null
-  }
-}
